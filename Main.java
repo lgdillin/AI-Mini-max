35d8846f2aa549c483459f60d5cea619b6757efa
lgdillin <mailgd9@gmail.com>
2018-10-07 14:33:58 -0500

    finished?

--- Main.java
+++ Main.java
@@ -11,50 +11,69 @@ class GameState {
 }
 
 class Main {
+  static final char HUMAN_MOVE = 'X';
+  static final char COMPUTER_MOVE = 'O';
 
   // Compute the best move for the computer according to the Mini-Max algorithm
-  // int miniMax(GameState g, int depth, boolean maximizingPlayer) {
-  //   //if(depth == 0 || node == checkBoard()) { return heuristic; }
-  //   int score = checkState(g);
-  //   if(score == 1) return score - depth;
-  //   else if(score == -1) return score + depth;
-  //   else if(score != 3) return 0;
-  //
-  //   int bestValue;
-  //   if(maximizingPlayer) {
-  //     bestValue = -1000; // Supposed to be neg inf
-  //
-  //     for(int i = 0; i < g.board.length; ++i) {
-  //       bestValue = Math.max(bestValue, miniMax(g.board[i], depth - 1, false));
-  //     }
-  //
-  //     return bestValue;
-  //   } else { // Minimizing player
-  //     bestValue = 1000; // positive inf
-  //     for(int i = 0; i < g.board.length; ++i) {
-  //       bestValue = Math.min(bestValue, miniMax(g.board[i], depth - 1, true));
-  //     }
-  //
-  //     return bestValue;
-  //   }
-  // }
+  int miniMax(GameState g, int depth, boolean maximizeUtility) {
+    //if(depth == 0 || node == checkBoard()) { return heuristic; }
+    int score = checkState(g);
+    if(score == 1) return score + depth;
+    else if(score == -1) return score - depth;
+    else if(score != 3) return 0;
+
+    int bestValue;
+    if(maximizeUtility) {
+      bestValue = -1000; // Supposed to be neg inf
+
+      for(int i = 0; i < g.board.length; ++i) {
+        if(g.board[i] == Character.forDigit(i + 1, 10)) {
+          g.board[i] = COMPUTER_MOVE;
+          bestValue = Math.max(bestValue, miniMax(g, depth + 1, !maximizeUtility));
+          g.board[i] = Character.forDigit(i + 1, 10);
+        }
+      }
+
+      return bestValue;
+    } else { // Minimizing player
+      bestValue = 1000; // positive inf
+      for(int i = 0; i < g.board.length; ++i) {
+        if(g.board[i] == Character.forDigit(i + 1, 10)) {
+          g.board[i] = HUMAN_MOVE;
+          bestValue = Math.min(bestValue, miniMax(g, depth + 1, !maximizeUtility));
+          g.board[i] = Character.forDigit(i + 1, 10);
+        }
+      }
+
+      return bestValue;
+    }
+  }
 
   // Returns the computer's best move for use in the actual game
   int bestMove(GameState g) {
     int maxUtility = -1000;
     int maxMove = -1;
     for(int i = 0; i < g.board.length; ++i) {
+      if(g.board[i] == Character.forDigit(i + 1, 10)) {
+        g.board[i] = COMPUTER_MOVE;
+        int utility = miniMax(g, 0, false);
+        g.board[i] = Character.forDigit(i + 1, 10);
 
+        if(utility > maxUtility) {
+          maxMove = i;
+          maxUtility = utility;
+        }
+      }
     }
     return maxMove;
   }
 
   // Updates the gameboard with the new value
   boolean updateBoard(char[] board, boolean human, int move) {
-    if(board[move] == 'X' || board[move] == 'O') return false;
+    if(board[move] == HUMAN_MOVE || board[move] == COMPUTER_MOVE) return false;
     else {
-      if(human) board[move] = 'X';
-      else board[move] = 'O';
+      if(human) board[move] = HUMAN_MOVE;
+      else board[move] = COMPUTER_MOVE;
     }
     return true;
   }
@@ -62,23 +81,23 @@ class Main {
   // Returns true if all positions in the game board are filled
   boolean boardFull(char[] board) {
     for(int i = 0; i < board.length; ++i) {
-      if( !(board[i] == 'X') && !(board[i] == 'O') ) return false; // Game not over
+      if( !(board[i] == HUMAN_MOVE) && !(board[i] == COMPUTER_MOVE) ) return false; // Game not over
     }
     return true;
   }
 
   // Checks for a terminal game state
   int checkState(GameState g) {
-    if(g.board[0] == g.board[1] && g.board[1] == g.board[2]) { if(g.board[0] == 'X') return 1; else return -1; }
-    if(g.board[3] == g.board[4] && g.board[4] == g.board[5]) { if(g.board[0] == 'X') return 1; else return -1; }
-    if(g.board[6] == g.board[7] && g.board[7] == g.board[8]) { if(g.board[0] == 'X') return 1; else return -1; }
+    if(g.board[0] == g.board[1] && g.board[1] == g.board[2]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
+    if(g.board[3] == g.board[4] && g.board[4] == g.board[5]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
+    if(g.board[6] == g.board[7] && g.board[7] == g.board[8]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
 
-    if(g.board[0] == g.board[3] && g.board[3] == g.board[6]) { if(g.board[0] == 'X') return 1; else return -1; }
-    if(g.board[1] == g.board[4] && g.board[4] == g.board[7]) { if(g.board[0] == 'X') return 1; else return -1; }
-    if(g.board[2] == g.board[5] && g.board[5] == g.board[8]) { if(g.board[0] == 'X') return 1; else return -1; }
+    if(g.board[0] == g.board[3] && g.board[3] == g.board[6]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
+    if(g.board[1] == g.board[4] && g.board[4] == g.board[7]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
+    if(g.board[2] == g.board[5] && g.board[5] == g.board[8]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
 
-    if(g.board[0] == g.board[4] && g.board[4] == g.board[8]) { if(g.board[0] == 'X') return 1; else return -1; }
-    if(g.board[6] == g.board[4] && g.board[4] == g.board[2]) { if(g.board[0] == 'X') return 1; else return -1; }
+    if(g.board[0] == g.board[4] && g.board[4] == g.board[8]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
+    if(g.board[6] == g.board[4] && g.board[4] == g.board[2]) { if(g.board[0] == HUMAN_MOVE) return 1; else return -1; }
 
     if(!boardFull(g.board)) return 3; // Game not over
     else return 0; // Tie Game
@@ -103,8 +122,8 @@ class Main {
     boolean human = true; // Let the human go first
 
     // Game loop
+    main.drawBoard(g.board);
     while(main.checkState(g) == 3) {
-      main.drawBoard(g.board);
 
       // Either the human plays or the computer
       int move = -1;
@@ -112,12 +131,12 @@ class Main {
         System.out.print("Your move: ");
         move = keyboard.nextInt();
       } else {
-        // move = ComputerMove() + 1;
-        move = r.nextInt(9) + 1;
+        move = main.bestMove(g) + 1;
       }
       System.out.println();
 
       if(!main.updateBoard(g.board, human, move - 1)) continue;
+      main.drawBoard(g.board);
       human = !human; // take turns
     }
 
